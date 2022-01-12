157bec02fccfd1c41abdb9eb1439624faaa92a78
nareha <nareh.agazaryan@yahoo.com>
2022-01-11 20:32:38 -0800

    Styles refactoring & Minor page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="terminal-container">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          guest@nareh.io:~$ neofetch
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
