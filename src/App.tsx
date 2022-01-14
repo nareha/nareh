0f7bfa74fceef0aab4e23d8a648586d1a75a7ef8
nareha <nareh.agazaryan@yahoo.com>
2022-01-14 01:31:10 -0800

    Added naive input functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/global.css';
 
+/* Art for display, to be added later */
+var ascii = [
+  "                                888",
+  "                                888",
+  "                                888",
+  ".d8888b   8888b.  88888b.d88b. 88888b.  888  .d88b.",
+  "88K          \"88b 888 \"888 \"88b 888 \"88b 888 d8P  Y8b",
+  "\"Y8888b. .d888888 888  888  888 888  888 888 88888888",
+  "    X88 888  888 888  888  888 888 d88P 888 Y8b.   ",
+  "  88888P' \"Y888888 888  888  888 88888P\"  888  \"Y8888  ",
+  "                                888                   ",
+  "                                888                   ",
+  "                                888                   ",
+].join('\n');
+
 function App() {
+  /* Elementary testing of command input */
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputHandler = (event : React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log("Command accepted")
+    }
+  }
+
   return (
     <div className="App">
       <div className="terminal-container">
         <p>
           guest@nareh.io:~$ neofetch
         </p>
+        <pre>
+          <code>
+            {ascii}
+          </code>
+        </pre>
+        <input
+          type="text"
+          value={inputValue}
+          onKeyDown={inputHandler}
+          onChange={(
+            ev: React.ChangeEvent<HTMLInputElement>,
+          ): void => setInputValue(ev.target.value)}
+        />           
       </div>
     </div>
   );
