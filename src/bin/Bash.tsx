7555b837eabcb9074f3bd35ed93b0f4f941e8a7c
nareha <nareh.agazaryan@yahoo.com>
2022-02-18 17:42:03 -0800

    feat: bash command added

---
+++ src/bin/Bash.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const Bash: FC = () => {
+    return (
+        <div>nope. zsh only here.</div>
+    );
+};
--- src/components/Command.tsx
+++ src/components/Command.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Ping } from '../bin/Ping';
 import { About } from '../bin/About';
+import { Bash } from '../bin/Bash';
 
 interface CommandProps {
     commandInput: string;
@@ -12,9 +13,11 @@ export const Command: FC<CommandProps> = ({ commandInput }) => {
             return <Ping />
         case "about":
             return <About />
+        case "bash":
+            return <Bash />
         default:
             return (
-                <div>Not a Command</div>
+                <div>zsh: command not found: {commandInput}</div>
             )
     }
 };
